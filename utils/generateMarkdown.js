217def0c2cdb2017dd1903e424a4c98fc80d70dc
Benoit <fbenoit2077@gmail.com>
2023-12-05 20:47:15 -0500

    renderLicenseBadge function added return markdown string, renderLicenseLink function return string

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  // Example for a generic link. You can customize it based on the license
+  return `[License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
