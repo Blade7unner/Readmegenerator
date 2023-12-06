6597426a0488773b08a62948df9bb45b891c9987
Benoit <fbenoit2077@gmail.com>
2023-12-05 20:22:14 -0500

    function to initialize app

--- index.js
+++ index.js
@@ -28,7 +28,11 @@ function writeToFile(fileName, data) {
   }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+      writeToFile('README.md', generateMarkdown(answers));
+    });
+  }
 
 // Function call to initialize app
 init();
