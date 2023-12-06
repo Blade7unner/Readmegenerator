14247fed2f884e0ade00dbdf48904531458144aa
Benoit <fbenoit2077@gmail.com>
2023-12-05 20:50:58 -0500

    added function to return text for the license sec of README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,14 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'None') {
+    return '';
+  }
+  return `## License
+
+This project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
